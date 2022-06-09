adc2235acdf274bbfca4adcb092d7ae1ceafc395
yuhakimura <yuhakimura.forstudy921@gmail.com>
2022-06-10 01:43:08 +0900

    example-openbox以外

--- js/main.js
+++ js/main.js
@@ -1,15 +1,3 @@
-var navPos = jQuery( '#global-nav' ).offset().top; // グローバルメニューの位置
-var navHeight = jQuery( '#global-nav' ).outerHeight(); // グローバルメニューの高さ
-jQuery( window ).on( 'scroll', function() {
-	if ( jQuery( this ).scrollTop() > navPos ) {
-		jQuery( 'body' ).css( 'padding-top', navHeight );
-		jQuery( '#global-nav' ).addClass( 'm_fixed' );
-	} else {
-		jQuery( 'body' ).css( 'padding-top', 0 );
-		jQuery( '#global-nav' ).removeClass( 'm_fixed' );
-	}
-});
-
 $(function($){
 	$('.more-button').on('click',function(){
         if ($('.open-box').is(':hidden')){
@@ -23,3 +11,15 @@ $(function($){
         }
     });
 })
+
+var navPos = jQuery( '#global-nav' ).offset().top; // グローバルメニューの位置
+var navHeight = jQuery( '#global-nav' ).outerHeight(); // グローバルメニューの高さ
+jQuery( window ).on( 'scroll', function() {
+	if ( jQuery( this ).scrollTop() > navPos ) {
+		jQuery( 'body' ).css( 'padding-top', navHeight );
+		jQuery( '#global-nav' ).addClass( 'm_fixed' );
+	} else {
+		jQuery( 'body' ).css( 'padding-top', 0 );
+		jQuery( '#global-nav' ).removeClass( 'm_fixed' );
+	}
+});
