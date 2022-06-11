2885430bb8ca4c92da0a9f19495e464d32234afb
yuhakimura <yuhakimura.forstudy921@gmail.com>
2022-06-12 03:38:05 +0900

    レスポンシブ途中まで6/11

--- js/main.js
+++ js/main.js
@@ -14,12 +14,12 @@ $(function($){
 	$('.answer-button').on('click',function(){
         if ($('.open-box').is(':hidden')){
             $('.open-box').slideDown();
-            $(this).text('閉じる');
+            $(this).html('&minus; 閉じる');
             $(this).css('background-color','#fff');
 			$(this).css('color','#000')
         } else{
             $('.open-box').slideUp();
-            $(this).text('+ 正解');
+            $(this).html('&plus; 正解');
             $(this).css('background-color', '#000');
 			$(this).css('color','#fff')
         }
